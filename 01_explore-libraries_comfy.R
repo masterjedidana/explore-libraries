51ff666cb061ecc849b22e48ca62b3a8ed3455dc
Dana Gibbon <dana_gibbon@live.com>
2018-01-31 16:07:02 -0600

    html

---
+++ 01_explore-libraries_comfy.R
@@ -0,0 +1,87 @@
+
+library(usethis)
+
+#' Which libraries does R search for packages?
+.libPaths()
+.Library # defualt library 
+
+library(fs)
+path_real(.Library) # show the true path (not sympolic link)
+
+# try .libPaths(), .Library
+
+
+#' Installed packages
+
+## use installed.packages() to get all installed packages
+## if you like working with data frame or tibble, make it so right away!
+## remember to use View() or similar to inspect
+head(installed.packages())
+pk <- as.data.frame(installed.packages())
+
+## how many packages?
+class(pk)
+nrow(pk)
+
+#' Exploring the packages
+
+## count some things! inspiration
+##   * tabulate by LibPath, Priority, or both
+summary(pk$LibPath)
+summary(pk$Priority)
+summary(pk$Version)
+##   * what proportion need compilation?
+##   * how break down re: version of R they were built on
+
+library(dplyr)
+## for tidyverts, here are some useful patterns
+# data %>% count(var)
+pk %>% count(LibPath)
+pk %>% count(LibPath, Priority)
+# data %>% count(var1, var2)
+# data %>% count(var) %>% mutate(prop = n / sum(n))
+
+##   * what proportion need compilation? # have C++?
+pk %>%
+  count(NeedsCompilation) %>%
+  mutate(prop = n / sum(n))
+##   * how break down re: version of R they were built on
+# how recently were the updated?
+pk %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
+#' Reflections
+
+## reflect on ^^ and make a few notes to yourself; inspiration
+##   * does the number of base + recommended packages make sense to you?
+all_default_pkgs <- list.files(.Library)
+all_br_pkgs <- pk %>%
+  filter(Priority %in% c("base", "recommended")) %>%
+  pull(Package)
+setdiff(all_default_pkgs, all_br_pkgs)
+
+##   * how does the result of .libPaths() relate to the result of .Library?
+
+ipt2 <- installed.packages(fields = "URL") %>%
+  as_tibble()
+ipt2 %>%
+  mutate(github = grepl("github", URL)) %>%
+  count(github) %>%
+  mutate(prop = n / sum(n))
+
+#' Going further
+
+## if you have time to do more ...
+
+## is every package in .Library either base or recommended?
+## study package naming style (all lower case, contains '.', etc
+## use `fields` argument to installed.packages() to get more info and use it!
+
+
+
+
+
+
+
+
